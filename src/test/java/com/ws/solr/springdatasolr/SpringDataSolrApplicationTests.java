b9afaa3a863bc56d3ef15fe2ec07ea25b2275cdd
Nitesh Kumar N <niteshkumarn55@gmail.com>
2020-03-19 12:04:41 +0530

    First commit

---
+++ src/test/java/com/ws/solr/springdatasolr/SpringDataSolrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.solr.springdatasolr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSolrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
